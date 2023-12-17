daed88ef57153c6e2ce31caf9f69416576998123
hiderr <szymon@hajderek.pl>
2023-12-17 15:48:46 +0100

    Updates

---
+++ assets/sample-retriever-processor-gy3Xts1G.js
@@ -0,0 +1,19 @@
+const REFRESH_COUNT = 60;
+class SampleRetrieverProcessor extends AudioWorkletProcessor {
+  prev_time = currentTime;
+  sample_chunks = [];
+  process(inputs, outputs, parameters) {
+    if (!inputs.length)
+      return false;
+    if (!inputs[0].length)
+      return false;
+    this.sample_chunks.push(inputs[0][0]);
+    while (currentTime - this.prev_time > 1 / REFRESH_COUNT) {
+      this.port.postMessage(this.sample_chunks);
+      this.sample_chunks.splice(0, this.sample_chunks.length);
+      this.prev_time = currentTime;
+    }
+    return true;
+  }
+}
+registerProcessor("sample-retriever", SampleRetrieverProcessor);
